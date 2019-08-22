949f1d2909c676678a31e5673a0207db42072ad3
amosfong <amos.fong@liferay.com>
2019-08-22 16:38:13 -0700

    LHC-1158

---
+++ modules/dxp/apps/osb/osb-koroneiki/osb-koroneiki-data-migration/src/main/java/com/liferay/osb/koroneiki/data/migration/internal/portlet/action/MigrateDataMVCActionCommand.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.koroneiki.data.migration.internal.portlet.action;
+
+import com.liferay.osb.koroneiki.data.migration.internal.constants.DataMigrationPortletKeys;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Amos Fong
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + DataMigrationPortletKeys.ADMIN,
+		"mvc.command.name=/migrate_data"
+	},
+	service = MVCActionCommand.class
+)
+public class MigrateDataMVCActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		try {
+			sendRedirect(actionRequest, actionResponse);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw e;
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		MigrateDataMVCActionCommand.class);
+
+}
