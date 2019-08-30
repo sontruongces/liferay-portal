34eafa8e7a7b3c3bed7dfb22fab3d81f7098a867
amosfong <amos.fong@liferay.com>
2019-08-30 12:26:54 -0700

    LHC-1040 add API for contact search

--- modules/dxp/apps/osb/osb-koroneiki/osb-koroneiki-phloem/osb-koroneiki-phloem-rest-impl/src/main/java/com/liferay/osb/koroneiki/phloem/rest/internal/resource/v1_0/ContactResourceImpl.java
+++ modules/dxp/apps/osb/osb-koroneiki/osb-koroneiki-phloem/osb-koroneiki-phloem-rest-impl/src/main/java/com/liferay/osb/koroneiki/phloem/rest/internal/resource/v1_0/ContactResourceImpl.java
@@ -16,15 +16,24 @@ package com.liferay.osb.koroneiki.phloem.rest.internal.resource.v1_0;
 
 import com.liferay.osb.koroneiki.phloem.rest.dto.v1_0.Contact;
 import com.liferay.osb.koroneiki.phloem.rest.dto.v1_0.util.ContactUtil;
+import com.liferay.osb.koroneiki.phloem.rest.internal.odata.entity.v1_0.ContactEntityModel;
 import com.liferay.osb.koroneiki.phloem.rest.resource.v1_0.ContactResource;
 import com.liferay.osb.koroneiki.taproot.service.ContactLocalService;
 import com.liferay.osb.koroneiki.taproot.service.ContactService;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.search.filter.Filter;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.odata.entity.EntityModel;
 import com.liferay.portal.vulcan.pagination.Page;
 import com.liferay.portal.vulcan.pagination.Pagination;
+import com.liferay.portal.vulcan.resource.EntityModelResource;
+import com.liferay.portal.vulcan.util.SearchUtil;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.ws.rs.core.MultivaluedMap;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -37,7 +46,8 @@ import org.osgi.service.component.annotations.ServiceScope;
 	properties = "OSGI-INF/liferay/rest/v1_0/contact.properties",
 	scope = ServiceScope.PROTOTYPE, service = ContactResource.class
 )
-public class ContactResourceImpl extends BaseContactResourceImpl {
+public class ContactResourceImpl
+	extends BaseContactResourceImpl implements EntityModelResource {
 
 	@Override
 	public void deleteContactByOkta(String oktaId) throws Exception {
@@ -86,6 +96,32 @@ public class ContactResourceImpl extends BaseContactResourceImpl {
 	public Contact getContactByUuid(String uuid) throws Exception {
 		return ContactUtil.toContact(
 			_contactService.getContactByUuid(uuid), null);
+	}
+
+	@Override
+	public Page<Contact> getContactsPage(
+			String search, Filter filter, Pagination pagination, Sort[] sorts)
+		throws Exception {
+
+		return SearchUtil.search(
+			booleanQuery -> {
+			},
+			filter, com.liferay.osb.koroneiki.taproot.model.Contact.class,
+			search, pagination,
+			queryConfig -> queryConfig.setSelectedFieldNames(
+				Field.ENTRY_CLASS_PK),
+			searchContext -> searchContext.setCompanyId(
+				contextCompany.getCompanyId()),
+			document -> ContactUtil.toContact(
+				_contactLocalService.getContact(
+					GetterUtil.getLong(document.get(Field.ENTRY_CLASS_PK))),
+				null),
+			sorts);
+	}
+
+	@Override
+	public EntityModel getEntityModel(MultivaluedMap multivaluedMap) {
+		return _entityModel;
 	}
 
 	@Override
@@ -141,6 +177,8 @@ public class ContactResourceImpl extends BaseContactResourceImpl {
 				contact.getEmailAddress(), languageId),
 			null);
 	}
+
+	private static final EntityModel _entityModel = new ContactEntityModel();
 
 	@Reference
 	private ContactLocalService _contactLocalService;
