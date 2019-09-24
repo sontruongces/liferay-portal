f8550536f2feef07e36d6f36bd05dc4231edbe1d
Jonathan McCann <jonathan.mccann@liferay.com>
2019-09-24 13:46:01 -0700

    LPS-102235 When adding a new group get a valid friendly URL instead of throwing an exception and returning. This is most clearly seen by adding a user whose screen name ends in a special URL character

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -385,7 +385,7 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 
 		validateInheritContent(parentGroupId, inheritContent);
 
-		validateFriendlyURL(
+		friendlyURL = getValidatedFriendlyURL(
 			user.getCompanyId(), groupId, classNameId, classPK, friendlyURL);
 
 		validateParentGroup(groupId, parentGroupId);
