701211b17911e5888da44185d8a773dd130a343b
Olivia Yu <olivia.yu@liferay.com>
2019-10-23 12:40:45 -0700

    LPS-99924 portal-search-tuning-rankings-web: Stop showing blank titles

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/results/builder/RankingJSONBuilder.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/results/builder/RankingJSONBuilder.java
@@ -29,7 +29,7 @@ public class RankingJSONBuilder {
 	public JSONObject build() {
 		return build(
 			JSONUtil.put(
-				"author", _document.getString(Field.USER_NAME)
+				"author", getAuthor()
 			).put(
 				"clicks", _document.getString("clicks")
 			).put(
@@ -73,6 +73,16 @@ public class RankingJSONBuilder {
 		return jsonObject;
 	}
 
+	protected String getAuthor() {
+		String entryClassName = _document.getString(Field.ENTRY_CLASS_NAME);
+
+		if (entryClassName.equals("com.liferay.portal.kernel.model.User")) {
+			return _document.getString("screenName");
+		}
+
+		return _document.getString(Field.USER_NAME);
+	}
+
 	protected String getTitle() {
 		String title = _document.getString(Field.TITLE + "_en_US");
 
@@ -80,7 +90,19 @@ public class RankingJSONBuilder {
 			return title;
 		}
 
-		return _document.getString(Field.TITLE);
+		title = _document.getString(Field.TITLE);
+
+		if (!Validator.isBlank(title)) {
+			return title;
+		}
+
+		String entryClassName = _document.getString(Field.ENTRY_CLASS_NAME);
+
+		if (entryClassName.equals("com.liferay.portal.kernel.model.User")) {
+			return _document.getString("fullName");
+		}
+
+		return _document.getString("name");
 	}
 
 	private Document _document;
