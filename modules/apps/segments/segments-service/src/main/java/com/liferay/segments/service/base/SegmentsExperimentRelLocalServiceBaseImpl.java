b6f34df8db963fc50faf01669dbdc27d1ac278eb
Eduardo Garcia <eduardo.garcia@liferay.com>
2019-10-28 15:04:16 +0100

    LPS-103409 Regen

--- modules/apps/segments/segments-service/src/main/java/com/liferay/segments/service/base/SegmentsExperimentRelLocalServiceBaseImpl.java
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/service/base/SegmentsExperimentRelLocalServiceBaseImpl.java
@@ -40,6 +40,7 @@ import com.liferay.segments.model.SegmentsExperimentRel;
 import com.liferay.segments.service.SegmentsExperimentRelLocalService;
 import com.liferay.segments.service.persistence.SegmentsEntryPersistence;
 import com.liferay.segments.service.persistence.SegmentsEntryRelPersistence;
+import com.liferay.segments.service.persistence.SegmentsEntryRolePersistence;
 import com.liferay.segments.service.persistence.SegmentsExperiencePersistence;
 import com.liferay.segments.service.persistence.SegmentsExperimentFinder;
 import com.liferay.segments.service.persistence.SegmentsExperimentPersistence;
@@ -419,6 +420,9 @@ public abstract class SegmentsExperimentRelLocalServiceBaseImpl
 
 	@Reference
 	protected SegmentsEntryRelPersistence segmentsEntryRelPersistence;
+
+	@Reference
+	protected SegmentsEntryRolePersistence segmentsEntryRolePersistence;
 
 	@Reference
 	protected SegmentsExperiencePersistence segmentsExperiencePersistence;
