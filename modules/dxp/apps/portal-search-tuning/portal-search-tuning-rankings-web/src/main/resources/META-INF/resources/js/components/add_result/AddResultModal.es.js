dc53030a0eb3b05ec2faea10654ce307cf759bbc
Olivia Yu <olivia.yu@liferay.com>
2019-11-14 15:34:27 -0800

    LPS-101278 portal-search-tuning-rankings-web: Update console log patterns

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/resources/META-INF/resources/js/components/add_result/AddResultModal.es.js
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/resources/META-INF/resources/js/components/add_result/AddResultModal.es.js
@@ -275,7 +275,7 @@ function AddResultModal({
 					description={Liferay.Language.get(
 						'search-the-engine-to-display-results'
 					)}
-					displayState="empty"
+					displayState={DISPLAY_STATES.EMPTY}
 					title={Liferay.Language.get('search-the-engine')}
 				/>
 			);
