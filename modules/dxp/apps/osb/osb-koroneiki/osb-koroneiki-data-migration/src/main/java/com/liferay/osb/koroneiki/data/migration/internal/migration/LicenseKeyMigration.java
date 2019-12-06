6f6efa3a44328cab157bfef9df59054d62317b35
amosfong <amos.fong@liferay.com>
2019-12-06 15:05:49 -0800

    LHC-1576 SF

--- modules/dxp/apps/osb/osb-koroneiki/osb-koroneiki-data-migration/src/main/java/com/liferay/osb/koroneiki/data/migration/internal/migration/LicenseKeyMigration.java
+++ modules/dxp/apps/osb/osb-koroneiki/osb-koroneiki-data-migration/src/main/java/com/liferay/osb/koroneiki/data/migration/internal/migration/LicenseKeyMigration.java
@@ -14,32 +14,23 @@
 
 package com.liferay.osb.koroneiki.data.migration.internal.migration;
 
+import com.liferay.osb.koroneiki.root.service.ExternalLinkLocalService;
 import com.liferay.osb.koroneiki.taproot.model.Account;
 import com.liferay.osb.koroneiki.taproot.service.AccountLocalService;
+import com.liferay.osb.koroneiki.trunk.model.ProductConsumption;
 import com.liferay.osb.koroneiki.trunk.model.ProductEntry;
-import com.liferay.osb.koroneiki.trunk.model.ProductField;
 import com.liferay.osb.koroneiki.trunk.service.ProductConsumptionLocalService;
 import com.liferay.osb.koroneiki.trunk.service.ProductEntryLocalService;
-import com.liferay.osb.koroneiki.trunk.service.ProductFieldLocalService;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.search.Document;
-import com.liferay.portal.kernel.search.Field;
-import com.liferay.portal.kernel.search.Hits;
-import com.liferay.portal.kernel.service.UserLocalService;
-import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringBundler;
-import com.liferay.portal.kernel.util.Validator;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -51,27 +42,20 @@ import org.osgi.service.component.annotations.Reference;
 public class LicenseKeyMigration {
 
 	public void migrate(long userId) throws Exception {
-		User user = _userLocalService.getUser(userId);
+		StringBundler sb = new StringBundler(7);
 
-		StringBundler sb = new StringBundler(10);
-
-		sb.append("select corpProjectId, OSB_ProductEntry.name, maxServers, ");
-		sb.append("maxHttpSessions, description, serverId, key_, startDate, ");
-		sb.append("expirationDate, complimentary, active_, ");
-		sb.append("maxConcurrentUsers, maxUsers, hostName, ipAddresses, ");
-		sb.append("macAddresses, additionalInfo, sizing from OSB_LicenseKey ");
-		sb.append("inner join OSB_AccountEntry on ");
+		sb.append("select corpProjectId, OSB_ProductEntry.name, licenseKeyId ");
+		sb.append("from OSB_LicenseKey inner join OSB_AccountEntry on ");
 		sb.append("OSB_LicenseKey.accountEntryId = ");
 		sb.append("OSB_AccountEntry.accountEntryId inner join ");
 		sb.append("OSB_ProductEntry on OSB_LicenseKey.productEntryId = ");
-		sb.append("OSB_ProductEntry.productEntryId where productId = 'Portal'");
+		sb.append("OSB_ProductEntry.productEntryId where productId = ");
+		sb.append("'Portal' and complimentary = 0 and active_ = 1");
 
 		try (Connection connection = DataAccess.getConnection();
 			PreparedStatement preparedStatement = connection.prepareStatement(
 				sb.toString());
 			ResultSet resultSet = preparedStatement.executeQuery()) {
-
-			ResultSetMetaData metaData = resultSet.getMetaData();
 
 			while (resultSet.next()) {
 				long corpProjectId = resultSet.getLong(1);
@@ -89,52 +73,28 @@ public class LicenseKeyMigration {
 
 				String productEntryName = resultSet.getString(2);
 
-				Hits hits = _productEntryLocalService.search(
-					user.getCompanyId(), productEntryName, 0, 1, null);
-
-				long productEntryId = 0;
-
-				ProductEntry productEntry = null;
-
-				for (Document document : hits.getDocs()) {
-					productEntryId = GetterUtil.getLong(
-						document.get(Field.ENTRY_CLASS_PK));
-
-					productEntry = _productEntryLocalService.fetchProductEntry(
-						productEntryId);
-				}
+				ProductEntry productEntry =
+					_productEntryLocalService.fetchProductEntryByName(
+						productEntryName);
 
 				if (productEntry == null) {
 					_log.error(
-						"Unable to find product with productEntryId " +
-							productEntryId);
+						"Unable to find product with name " + productEntryName);
 
 					continue;
 				}
 
-				List<ProductField> productFields = new ArrayList<>();
+				ProductConsumption productConsumption =
+					_productConsumptionLocalService.addProductConsumption(
+						userId, corpProjectId, productEntry.getProductEntryId(),
+						Collections.emptyList());
 
-				for (int i = 3; i < 19; i++) {
-					ProductField productField =
-						_productFieldLocalService.createProductField(0);
+				long licenseKeyId = resultSet.getLong(3);
 
-					String value = resultSet.getString(i);
-
-					if (Validator.isNull(value)) {
-						continue;
-					}
-
-					String name = metaData.getColumnName(i);
-
-					productField.setName(name);
-
-					productField.setValue(value);
-
-					productFields.add(productField);
-				}
-
-				_productConsumptionLocalService.addProductConsumption(
-					userId, corpProjectId, productEntryId, productFields);
+				_externalLinkLocalService.addExternalLink(
+					userId, ProductConsumption.class.getName(),
+					productConsumption.getProductConsumptionId(),
+					"provisioning", "license", String.valueOf(licenseKeyId));
 			}
 		}
 	}
@@ -146,15 +106,12 @@ public class LicenseKeyMigration {
 	private AccountLocalService _accountLocalService;
 
 	@Reference
+	private ExternalLinkLocalService _externalLinkLocalService;
+
+	@Reference
 	private ProductConsumptionLocalService _productConsumptionLocalService;
 
 	@Reference
 	private ProductEntryLocalService _productEntryLocalService;
-
-	@Reference
-	private ProductFieldLocalService _productFieldLocalService;
-
-	@Reference
-	private UserLocalService _userLocalService;
 
 }
