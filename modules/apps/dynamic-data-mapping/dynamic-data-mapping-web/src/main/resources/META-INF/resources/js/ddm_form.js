29489c150176b61c693d6b1d613f85797d93f47b
Vendel Toreki <vendel.toreki@liferay.com>
2019-12-12 12:44:45 +0100

    LPS-105572 Handle the case when the user filled the form already: write back the original value before trying the default one

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
@@ -1033,6 +1033,10 @@ AUI.add(
 						}
 
 						if (Lang.isUndefined(value)) {
+							value = instance.getValue();
+						}
+
+						if (Lang.isUndefined(value)) {
 							value = instance.getDefaultLocalization(
 								instance.get('displayLocale')
 							);
