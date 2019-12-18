bc98e5918cedd9ee5c34ea39aac292ace3f9e1b0
Christopher Kian <christopher.kian@liferay.com>
2019-12-18 11:42:44 -0800

    LPS-100082 Change logic for fetching live group ID to avoid adding reference and empty try/catch

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/base/JournalArticleLocalServiceBaseImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/base/JournalArticleLocalServiceBaseImpl.java
@@ -663,9 +663,6 @@ public abstract class JournalArticleLocalServiceBaseImpl
 		counterLocalService;
 
 	@Reference
-	protected com.liferay.exportimport.kernel.staging.Staging staging;
-
-	@Reference
 	protected com.liferay.portal.kernel.service.ClassNameLocalService
 		classNameLocalService;
 
