f5cfb536168db2105e1c3fd0d9a0f5378b1e4f80
ChrisKian <christopher.kian@liferay.com>
2019-12-18 12:36:55 -0800

    LPS-100082 add dependencies 72x unique commit

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/exportimport/data/handler/JournalArticleStagedModelDataHandler.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/exportimport/data/handler/JournalArticleStagedModelDataHandler.java
@@ -96,6 +96,7 @@ import java.io.InputStream;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
