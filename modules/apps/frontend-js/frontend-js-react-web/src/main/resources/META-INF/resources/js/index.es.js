ab9ba2f6db82b1da36b3e80a3d1ce4d5dc0f8d93
Antonio Ortega <60252917@liferay.com>
2020-01-07 10:26:22 +0100

    LPS-106044 Backport of 'LPS-103292 Add "useTimeout" and "useInterval" hooks'
    Original commit message: Add "useTimeout" and "useInterval" hooks cfee80c6a096454909e8c5692acfb2902e787bb9

--- modules/apps/frontend-js/frontend-js-react-web/src/main/resources/META-INF/resources/js/index.es.js
+++ modules/apps/frontend-js/frontend-js-react-web/src/main/resources/META-INF/resources/js/index.es.js
@@ -14,8 +14,10 @@
 
 export {default as render} from './render.es';
 export {default as useEventListener} from './hooks/useEventListener.es';
+export {default as useInterval} from './hooks/useInterval.es';
 export {default as useIsMounted} from './hooks/useIsMounted.es';
 export {default as usePrevious} from './hooks/usePrevious.es';
+export {default as useTimeout} from './hooks/useTimeout.es';
 
 // Egregious hack because react-dnd expects `window.process` to exist:
 //
