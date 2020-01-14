432f98c79f4aa66739f2490d16220cb906051cd2
Brian Chan <brian.chan@liferay.com>
2020-01-14 13:42:06 -0800

    LPS-106882 group by searchContext.* as before

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -5750,12 +5750,6 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 		if (params != null) {
 			attributes.put(
 				Field.GROUP_ID, (Long)params.getOrDefault("usersGroups", 0L));
-
-			String keywords = (String)params.remove("keywords");
-
-			if (Validator.isNotNull(keywords)) {
-				searchContext.setKeywords(keywords);
-			}
 		}
 
 		attributes.put("city", city);
@@ -5776,6 +5770,14 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 
 		searchContext.setCompanyId(companyId);
 		searchContext.setEnd(end);
+
+		if (params != null) {
+			String keywords = (String)params.remove("keywords");
+
+			if (Validator.isNotNull(keywords)) {
+				searchContext.setKeywords(keywords);
+			}
+		}
 
 		if (sorts != null) {
 			searchContext.setSorts(sorts);
