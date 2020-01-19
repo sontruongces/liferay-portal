7566d439ea2466fc2168919db011b8468a56657c
Javier Gamarra <nhpatt@gmail.com>
2020-01-19 20:20:30 +0100

    LPS-98387 add null check

--- modules/apps/portal-vulcan/portal-vulcan-api/src/main/java/com/liferay/portal/vulcan/util/ActionUtil.java
+++ modules/apps/portal-vulcan/portal-vulcan-api/src/main/java/com/liferay/portal/vulcan/util/ActionUtil.java
@@ -85,7 +85,7 @@ public class ActionUtil {
 				siteId, permissionName, id, actionName) ||
 			(OAuth2ProviderScopeLiferayAccessControlContext.
 				isOAuth2AuthVerified() &&
-			 !scopeChecker.checkScope(methodName))) {
+			 (scopeChecker != null) && !scopeChecker.checkScope(methodName))) {
 
 			return null;
 		}
