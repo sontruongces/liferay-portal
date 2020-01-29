bc0b41cdff1b2ace865923172c5c0b5a734fc0b7
Brian Chan <brian.chan@liferay.com>
2020-01-29 09:09:18 -0800

    LPS-102130 type: git grep processedHTML

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryLinkLocalServiceImpl.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryLinkLocalServiceImpl.java
@@ -195,7 +195,7 @@ public class FragmentEntryLinkLocalServiceImpl
 		fragmentEntryLink.setJs(js);
 		fragmentEntryLink.setConfiguration(configuration);
 
-		String processedHtml = html;
+		String processedHTML = html;
 
 		HttpServletRequest httpServletRequest = serviceContext.getRequest();
 		HttpServletResponse httpServletResponse = serviceContext.getResponse();
@@ -207,7 +207,7 @@ public class FragmentEntryLinkLocalServiceImpl
 					FragmentEntryLinkConstants.EDIT,
 					LocaleUtil.getMostRelevantLocale());
 
-			processedHtml =
+			processedHTML =
 				_fragmentEntryProcessorRegistry.processFragmentEntryLinkHTML(
 					fragmentEntryLink, fragmentEntryProcessorContext);
 		}
@@ -216,7 +216,7 @@ public class FragmentEntryLinkLocalServiceImpl
 			JSONObject jsonObject =
 				_fragmentEntryProcessorRegistry.
 					getDefaultEditableValuesJSONObject(
-						processedHtml, configuration);
+						processedHTML, configuration);
 
 			editableValues = jsonObject.toString();
 		}
