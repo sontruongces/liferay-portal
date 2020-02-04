a13ee6eab1f95089eba8736b5286d40d14200eb5
amosfong <amos.fong@liferay.com>
2020-02-04 15:34:42 -0800

    LHC-1677 fix entitlement audit entries

--- modules/dxp/apps/osb/osb-koroneiki/osb-koroneiki-root/osb-koroneiki-root-audit/src/main/java/com/liferay/osb/koroneiki/root/audit/model/listener/EntitlementModelListener.java
+++ modules/dxp/apps/osb/osb-koroneiki/osb-koroneiki-root/osb-koroneiki-root-audit/src/main/java/com/liferay/osb/koroneiki/root/audit/model/listener/EntitlementModelListener.java
@@ -19,6 +19,8 @@ import com.liferay.osb.koroneiki.phytohormone.service.EntitlementLocalService;
 import com.liferay.osb.koroneiki.root.audit.model.BaseAuditModelListener;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.StringPool;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -43,6 +45,23 @@ public class EntitlementModelListener
 	@Override
 	protected Entitlement getModel(long classPK) throws PortalException {
 		return _entitlementLocalService.getEntitlement(classPK);
+	}
+
+	@Override
+	protected ServiceContext getServiceContext(long classNameId, long classPK) {
+		ServiceContext serviceContext = super.getServiceContext(
+			classNameId, classPK);
+
+		serviceContext.setAttribute("agentName", StringPool.BLANK);
+
+		return serviceContext;
+	}
+
+	@Override
+	protected long getUserId() throws PortalException {
+		long companyId = portalInstancesLocalService.getDefaultCompanyId();
+
+		return userLocalService.getDefaultUserId(companyId);
 	}
 
 	@Reference
