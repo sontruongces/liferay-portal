f9267d69a67ab7f7c8c16e6ee7e7fd08a4c9b94c
Laszlo Pap <laszlo.pap@liferay.com>
2020-02-19 15:06:13 +0100

    LPS-108154 Breadcrumb portlet shows the unnecessary guest site entry in staging

--- portal-kernel/src/com/liferay/portal/kernel/servlet/taglib/ui/BreadcrumbUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/servlet/taglib/ui/BreadcrumbUtil.java
@@ -301,7 +301,7 @@ public class BreadcrumbUtil {
 			layoutsPageCount = group.getPublicLayoutsPageCount();
 		}
 
-		if ((layoutsPageCount > 0) && !group.isGuest()) {
+		if ((layoutsPageCount > 0) && !_checkIfCurrentSiteIsGuestSite(group)) {
 			String layoutSetFriendlyURL = PortalUtil.getLayoutSetFriendlyURL(
 				layoutSet, themeDisplay);
 
@@ -364,6 +364,26 @@ public class BreadcrumbUtil {
 		breadcrumbEntry.setURL(layoutURL);
 
 		breadcrumbEntries.add(breadcrumbEntry);
+	}
+
+	private static boolean _checkIfCurrentSiteIsGuestSite(Group group) {
+		if (group.isGuest()) {
+			return true;
+		}
+
+		if (group.isStaged()) {
+			Group liveGroup = group.getLiveGroup();
+
+			if (liveGroup != null) {
+				String groupKey = liveGroup.getGroupKey();
+
+				if (groupKey.equals(GroupConstants.GUEST)) {
+					return true;
+				}
+			}
+		}
+
+		return false;
 	}
 
 	private static LayoutSet _getParentLayoutSet(LayoutSet layoutSet)
