2fdf65fc5e2600b1f73794285558bb480227ee9a
Geyson Silva <geyson.silva@liferay.com>
2020-02-20 17:12:26 -0300

    LRAC-5187 Return empty set

--- modules/apps/analytics/analytics-settings-web/src/main/java/com/liferay/analytics/settings/web/internal/display/context/GroupDisplayContext.java
+++ modules/apps/analytics/analytics-settings-web/src/main/java/com/liferay/analytics/settings/web/internal/display/context/GroupDisplayContext.java
@@ -214,8 +214,8 @@ public class GroupDisplayContext {
 	}
 
 	private Set<String> _getDisabledGroupIds() {
-		if (!MapUtil.isEmpty(_channelNames)) {
-			Collections.emptySet();
+		if (MapUtil.isEmpty(_channelNames)) {
+			return Collections.emptySet();
 		}
 
 		return _channelNames.keySet();
