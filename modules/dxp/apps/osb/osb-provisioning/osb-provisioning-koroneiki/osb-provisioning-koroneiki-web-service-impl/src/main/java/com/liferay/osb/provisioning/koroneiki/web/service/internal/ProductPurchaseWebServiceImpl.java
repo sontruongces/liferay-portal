1b0d5cff06bb9604d4f40becd006310366b5fe23
kylembischof <kyle.bischof@liferay.com>
2020-02-20 08:55:39 -0800

    LHC-1883 edit productPurchases

---
+++ modules/dxp/apps/osb/osb-provisioning/osb-provisioning-koroneiki/osb-provisioning-koroneiki-web-service-impl/src/main/java/com/liferay/osb/provisioning/koroneiki/web/service/internal/ProductPurchaseWebServiceImpl.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.provisioning.koroneiki.web.service.internal;
+
+import com.liferay.osb.koroneiki.phloem.rest.client.dto.v1_0.ProductPurchase;
+import com.liferay.osb.koroneiki.phloem.rest.client.pagination.Page;
+import com.liferay.osb.koroneiki.phloem.rest.client.pagination.Pagination;
+import com.liferay.osb.koroneiki.phloem.rest.client.resource.v1_0.ProductPurchaseResource;
+import com.liferay.osb.provisioning.koroneiki.web.service.ProductPurchaseWebService;
+import com.liferay.osb.provisioning.koroneiki.web.service.internal.configuration.KoroneikiConfiguration;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Kyle Bischof
+ */
+@Component(
+	configurationPid = "com.liferay.osb.provisioning.koroneiki.web.service.internal.configuration.KoroneikiConfiguration",
+	immediate = true, service = ProductPurchaseWebService.class
+)
+public class ProductPurchaseWebServiceImpl
+	implements ProductPurchaseWebService {
+
+	public List<ProductPurchase> getProductPurchases(
+			String filterString, int page, int pageSize, String sortString)
+		throws Exception {
+
+		Page<ProductPurchase> productPurchasesPage =
+			_productPurchaseResource.getProductPurchasesPage(
+				StringPool.BLANK, filterString, Pagination.of(page, pageSize),
+				sortString);
+
+		if ((productPurchasesPage != null) &&
+			(productPurchasesPage.getItems() != null)) {
+
+			return new ArrayList<>(productPurchasesPage.getItems());
+		}
+
+		return Collections.emptyList();
+	}
+
+	public ProductPurchase putProductPurchase(
+			String agentName, String productPurchaseKey, 
+			ProductPurchase productPurchase)
+		throws Exception {
+
+		return _productPurchaseResource.putProductPurchase(
+			agentName, productPurchaseKey, productPurchase);
+	}
+
+	@Activate
+	protected void activate(Map<String, Object> properties) throws Exception {
+		KoroneikiConfiguration koroneikiConfiguration =
+			ConfigurableUtil.createConfigurable(
+				KoroneikiConfiguration.class, properties);
+
+		ProductPurchaseResource.Builder builder =
+			ProductPurchaseResource.builder();
+
+		_productPurchaseResource = builder.endpoint(
+			koroneikiConfiguration.host(), koroneikiConfiguration.port(),
+			koroneikiConfiguration.scheme()
+		).header(
+			"API_Token", koroneikiConfiguration.apiToken()
+		).parameter(
+			"nestedFields", "products"
+		).build();
+	}
+
+	private ProductPurchaseResource _productPurchaseResource;
+
+}
---
+++ modules/dxp/apps/osb/osb-provisioning/osb-provisioning-web/src/main/java/com/liferay/osb/provisioning/web/internal/portlet/action/EditProductPurchaseMVCActionCommand.java
@@ -0,0 +1,137 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.provisioning.web.internal.portlet.action;
+
+import com.liferay.osb.koroneiki.phloem.rest.client.dto.v1_0.ProductPurchase;
+import com.liferay.osb.provisioning.constants.ProvisioningPortletKeys;
+import com.liferay.osb.provisioning.koroneiki.web.service.ProductPurchaseWebService;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Kyle Bischof
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + ProvisioningPortletKeys.PROVISIONING,
+		"mvc.command.name=/product_purchases/edit_product_purchase"
+	},
+	service = MVCActionCommand.class
+)
+public class EditProductPurchaseMVCActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		try {
+			updateProductPurchase(actionRequest);
+
+			sendRedirect(actionRequest, actionResponse);
+		}
+		catch (Exception exception) {
+			_log.error(exception, exception);
+
+			throw exception;
+		}
+	}
+
+	protected void updateProductPurchase(ActionRequest actionRequest)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		User user = themeDisplay.getUser();
+
+		String productPurchaseKey = ParamUtil.getString(
+			actionRequest, "productPurchaseKey");
+
+		int startDateMonth = ParamUtil.getInteger(
+			actionRequest, "startDateMonth");
+		int startDateDay = ParamUtil.getInteger(actionRequest, "startDateDay");
+		int startDateYear = ParamUtil.getInteger(
+			actionRequest, "startDateYear");
+
+		Date startDate = _portal.getDate(
+			startDateMonth, startDateDay, startDateYear,
+			themeDisplay.getTimeZone(), null);
+
+		int endDateMonth = ParamUtil.getInteger(actionRequest, "endDateMonth");
+		int endDateDay = ParamUtil.getInteger(actionRequest, "endDateDay");
+		int endDateYear = ParamUtil.getInteger(actionRequest, "endDateYear");
+
+		Date endDate = _portal.getDate(
+			endDateMonth, endDateDay, endDateYear, themeDisplay.getTimeZone(),
+			null);
+
+		int originalEndDateMonth = ParamUtil.getInteger(
+			actionRequest, "originalEndDateMonth");
+		int originalEndDateDay = ParamUtil.getInteger(
+			actionRequest, "originalEndDateDay");
+		int originalEndDateYear = ParamUtil.getInteger(
+			actionRequest, "originalEndDateYear");
+
+		Date originalEndDate = _portal.getDate(
+			originalEndDateMonth, originalEndDateDay, originalEndDateYear,
+			themeDisplay.getTimeZone(), null);
+
+		int quantity = ParamUtil.getInteger(actionRequest, "quantity");
+		int sizing = ParamUtil.getInteger(actionRequest, "sizing");
+
+		Map<String, String> properties = new HashMap<>();
+
+		properties.put("sizing", String.valueOf(sizing));
+
+		ProductPurchase productPurchase = new ProductPurchase();
+
+		productPurchase.setStartDate(startDate);
+		productPurchase.setEndDate(endDate);
+		productPurchase.setOriginalEndDate(originalEndDate);
+		productPurchase.setQuantity(quantity);
+		productPurchase.setProperties(properties);
+
+		_productPurchaseWebService.putProductPurchase(
+			user.getFullName(), productPurchaseKey, productPurchase);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		EditProductPurchaseMVCActionCommand.class);
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private ProductPurchaseWebService _productPurchaseWebService;
+
+}
