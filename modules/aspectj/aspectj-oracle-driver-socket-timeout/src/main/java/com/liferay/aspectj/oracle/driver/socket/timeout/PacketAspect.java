sontruongces/liferay-portal

commit 68ee310fed41cdf62187b07e22ffe11bc7f080a4

LPS-105380 Rename exception variables with counters

modules/aspectj/aspectj-oracle-driver-socket-timeout/src/main/java/com/liferay/aspectj/oracle/driver/socket/timeout/PacketAspect.java
@@ -33,7 +33,7 @@ public class PacketAspect {
 				"args(interruptedIOException) && this(packet)"
 	)
 	public void addSuppressedInterruptedIOException(
-			Object packet, Exception interruptedIOException)
+			Object packet, Exception exception1)
 		throws Exception {
 
 		Class<?> clazz = packet.getClass();
@@ -45,11 +45,11 @@ public class PacketAspect {
 		Constructor<? extends Exception> constructor =
 			(Constructor<? extends Exception>)clazz.getConstructor(int.class);
 
-		Exception exception = constructor.newInstance(504);
+		Exception exception2 = constructor.newInstance(504);
 
-		exception.addSuppressed(interruptedIOException);
+		exception2.addSuppressed(exception1);
 
-		throw exception;
+		throw exception2;
 	}
 
 }

commit 1d8819a87fceb8d346b259aa4cb317df47772a10

LPS-108757 Need to change var name in pointcut too. Caused by 68ee310fed41cdf62187b07e22ffe11bc7f080a4

modules/aspectj/aspectj-oracle-driver-socket-timeout/src/main/java/com/liferay/aspectj/oracle/driver/socket/timeout/PacketAspect.java
@@ -30,7 +30,7 @@ public class PacketAspect {
 	@Before(
 		"handler(java.io.InterruptedIOException) &&" +
 			"withincode(void oracle.net.ns.Packet.receive()) &&" +
-				"args(interruptedIOException) && this(packet)"
+				"args(exception1) && this(packet)"
 	)
 	public void addSuppressedInterruptedIOException(
 			Object packet, Exception exception1)