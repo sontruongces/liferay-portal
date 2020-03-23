e2dce087f61a9bcbf4d01709332ed7d223d25f45
Steven Sun <yunlin.sun@liferay.com>
2020-03-23 13:49:57 +0800

    Revert "LPS-106508 optimize for the human brain"
    This reverts commit 711505a2fdc468c225a08592b9d32252f9090104.

--- modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/util/DLValidatorImpl.java
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/util/DLValidatorImpl.java
@@ -139,12 +139,14 @@ public final class DLValidatorImpl implements DLValidator {
 	public void validateFileExtension(String fileName)
 		throws FileExtensionException {
 
+		String fileNameExtension = StringUtil.toLowerCase(
+			FileUtil.getExtension(fileName));
+
 		boolean validFileExtension = false;
 
-		for (String fileExtension : _dlConfiguration.fileExtensions()) {
-			String fileNameExtension = StringUtil.toLowerCase(
-				FileUtil.getExtension(fileName));
+		String[] fileExtensions = _dlConfiguration.fileExtensions();
 
+		for (String fileExtension : fileExtensions) {
 			if (StringPool.STAR.equals(fileExtension) ||
 				StringUtil.equals(
 					fileNameExtension, StringUtil.toLowerCase(fileExtension))) {
