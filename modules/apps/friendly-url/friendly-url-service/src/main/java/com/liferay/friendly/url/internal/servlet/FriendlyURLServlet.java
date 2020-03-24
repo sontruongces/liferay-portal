73d2ed43d2518b46681e9146b13cd029f85788f7
Attila Bakay <attila.bakay@liferay.com>
2020-03-24 09:06:06 +0100

    LPS-110376 handle control_panel as an exception from inactive 404

--- modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/servlet/FriendlyURLServlet.java
+++ modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/servlet/FriendlyURLServlet.java
@@ -125,7 +125,8 @@ public class FriendlyURLServlet extends HttpServlet {
 
 		if ((group == null) ||
 			(!group.isActive() &&
-			 !inactiveRequestHandler.isShowInactiveRequestMessageEnabled())) {
+			 !inactiveRequestHandler.isShowInactiveRequestMessageEnabled() &&
+			 !path.contains("/control_panel/"))) {
 
 			StringBundler sb = new StringBundler(5);
 
