32136403f546e92d9b5dd8337374a8e5b0372a1b
Tibor Lipusz <tibor.lipusz@liferay.com>
2020-04-23 12:06:32 +0200

    LPS-110846 regen
    https://issues.liferay.com/browse/LPS-110846

--- portal-kernel/src/com/liferay/portal/kernel/service/GroupLocalServiceWrapper.java
+++ portal-kernel/src/com/liferay/portal/kernel/service/GroupLocalServiceWrapper.java
@@ -825,6 +825,13 @@ public class GroupLocalServiceWrapper
 		return _groupLocalService.getGroupByUuidAndCompanyId(uuid, companyId);
 	}
 
+	@Override
+	public java.util.List<java.lang.Long> getGroupIds(
+		long companyId, boolean active) {
+
+		return _groupLocalService.getGroupIds(companyId, active);
+	}
+
 	/**
 	 * Returns a range of all the groups.
 	 *
