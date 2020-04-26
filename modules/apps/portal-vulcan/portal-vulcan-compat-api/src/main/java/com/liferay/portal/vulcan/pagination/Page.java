abdfd7e235b696ae83abb58e15f8aab849f6a67c
Javier Gamarra <nhpatt@gmail.com>
2020-04-26 09:06:34 +0200

    LPS-112528 Vulcan compat module

---
+++ modules/apps/portal-vulcan/portal-vulcan-compat-api/src/main/java/com/liferay/portal/vulcan/pagination/Page.java
@@ -0,0 +1,150 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.vulcan.pagination;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Alejandro Hernández
+ * @author Ivica Cardic
+ * @author Brian Wing Shun Chan
+ */
+@JacksonXmlRootElement(localName = "page")
+public class Page<T> {
+
+	public static <T> Page<T> of(Collection<T> items) {
+		return new Page<>(items);
+	}
+
+	public static <T> Page<T> of(
+		Collection<T> items, Pagination pagination, long totalCount) {
+
+		return new Page<>(items, pagination, totalCount);
+	}
+
+	public static <T> Page<T> of(
+		Map<String, Map<String, String>> actions, Collection<T> items) {
+
+		return new Page<>(actions, items);
+	}
+
+	public static <T> Page<T> of(
+		Map<String, Map<String, String>> actions, Collection<T> items,
+		Pagination pagination, long totalCount) {
+
+		return new Page<>(actions, items, pagination, totalCount);
+	}
+
+	@JsonProperty("actions")
+	public Map<String, Map<String, String>> getActions() {
+		return _actions;
+	}
+
+	@JacksonXmlElementWrapper(localName = "items")
+	@JacksonXmlProperty(localName = "item")
+	public Collection<T> getItems() {
+		return new ArrayList<>(_items);
+	}
+
+	public long getLastPage() {
+		if ((_pageSize == 0) || (_totalCount == 0)) {
+			return 1;
+		}
+
+		return -Math.floorDiv(-_totalCount, _pageSize);
+	}
+
+	@JsonProperty("page")
+	public long getPage() {
+		return _page;
+	}
+
+	@JsonProperty("pageSize")
+	public long getPageSize() {
+		return _pageSize;
+	}
+
+	public long getTotalCount() {
+		return _totalCount;
+	}
+
+	public boolean hasNext() {
+		if (getLastPage() > _page) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean hasPrevious() {
+		if (_page > 1) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private Page(Collection<T> items) {
+		this(new HashMap<>(), items);
+	}
+
+	private Page(Collection<T> items, Pagination pagination, long totalCount) {
+		this(new HashMap<>(), items, pagination, totalCount);
+	}
+
+	private Page(
+		Map<String, Map<String, String>> actions, Collection<T> items) {
+
+		_actions = actions;
+		_items = items;
+		_page = 1;
+		_pageSize = items.size();
+		_totalCount = items.size();
+	}
+
+	private Page(
+		Map<String, Map<String, String>> actions, Collection<T> items,
+		Pagination pagination, long totalCount) {
+
+		_actions = actions;
+		_items = items;
+
+		if (pagination == null) {
+			_page = 0;
+			_pageSize = 0;
+		}
+		else {
+			_page = pagination.getPage();
+			_pageSize = pagination.getPageSize();
+		}
+
+		_totalCount = totalCount;
+	}
+
+	private final Map<String, Map<String, String>> _actions;
+	private final Collection<T> _items;
+	private final long _page;
+	private final long _pageSize;
+	private final long _totalCount;
+
+}
---
+++ modules/apps/portal-vulcan/portal-vulcan-compat-api/src/main/java/com/liferay/portal/vulcan/util/LocalizedMapUtil.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.vulcan.util;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class LocalizedMapUtil {
+
+	public static Map<String, String> getLocalizedMap(
+		boolean acceptAllLanguages, Map<Locale, String> localizedMap) {
+
+		if (!acceptAllLanguages) {
+			return null;
+		}
+
+		Map<String, String> map = new HashMap<>();
+
+		for (Map.Entry<Locale, String> entry : localizedMap.entrySet()) {
+			Locale locale = entry.getKey();
+
+			map.put(locale.toLanguageTag(), entry.getValue());
+		}
+
+		return map;
+	}
+
+	public static Map<Locale, String> merge(
+		Map<Locale, String> map, Map.Entry<Locale, String> entry) {
+
+		if (map == null) {
+			return Stream.of(
+				entry
+			).collect(
+				Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)
+			);
+		}
+
+		if (entry == null) {
+			return map;
+		}
+
+		if (entry.getValue() == null) {
+			map.remove(entry.getKey());
+
+			return map;
+		}
+
+		Set<Map.Entry<Locale, String>> mapEntries = map.entrySet();
+
+		return Stream.concat(
+			mapEntries.stream(), Stream.of(entry)
+		).collect(
+			Collectors.toMap(
+				Map.Entry::getKey, Map.Entry::getValue,
+				(value1, value2) -> value2)
+		);
+	}
+
+	public static Map<Locale, String> patch(
+		Map<Locale, String> map, Locale locale, String value) {
+
+		if (value != null) {
+			map.put(locale, value);
+		}
+
+		return map;
+	}
+
+}
