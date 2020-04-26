2b873802a545fd2ac560f4f9a54e0816063d0f04
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-04-26 17:04:58 +0200

    LPS-114239 Adds new property to enable or disable the ContentTransformerListener, by default is enabled

--- modules/apps/journal/journal-api/src/main/java/com/liferay/journal/configuration/JournalServiceConfiguration.java
+++ modules/apps/journal/journal-api/src/main/java/com/liferay/journal/configuration/JournalServiceConfiguration.java
@@ -151,4 +151,10 @@ public interface JournalServiceConfiguration {
 	)
 	public boolean singleAssetPublishIncludeVersionHistory();
 
+	@Meta.AD(
+		deflt = "true", name = "enable-content-transformer-listener",
+		required = false
+	)
+	public boolean enableContentTransformerListener();
+
 }
