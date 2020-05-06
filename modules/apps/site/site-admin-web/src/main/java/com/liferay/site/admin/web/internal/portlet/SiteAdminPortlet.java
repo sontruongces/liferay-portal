14610cbc6d7fd67a716357bb5b95eb00fdf13302
ricardocousolr <ricardo.couso@liferay.com>
2020-05-06 16:09:54 +0200

    LPS-113121 As for Guest group, do not validate default locale group name
    Don't display alert for Group name either since it cannot be changed anyway

--- modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/portlet/SiteAdminPortlet.java
+++ modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/portlet/SiteAdminPortlet.java
@@ -784,7 +784,7 @@ public class SiteAdminPortlet extends MVCPortlet {
 			Locale defaultLocale = LocaleUtil.fromLanguageId(
 				unicodeProperties.getProperty("languageId"));
 
-			if (!liveGroup.isGuest()) {
+			if (!liveGroup.isGuest() && !liveGroup.isOrganization()) {
 				validateDefaultLocaleGroupName(nameMap, defaultLocale);
 			}
 
