eea2f476ca4557ee8072cebdafc081b2e471f740
Marcell Gyöpös <marcell.gyopos@liferay.com>
2020-05-15 12:58:45 +0200

    LPS-112800 Modify the unlocalisable logic, now you can modify the element in every language but the value always be the same. Add defaultlanguage of the structure to availableLanguageIds when the portal is being used in a  different language.

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
@@ -358,17 +358,7 @@ AUI.add(
 				if (instance.get('readOnly')) {
 					retVal = true;
 				}
-				else {
-					var form = instance.getForm();
-
-					if (
-						!instance.get('localizable') &&
-						form.getDefaultLocale() != instance.get('displayLocale')
-					) {
-						retVal = true;
-					}
-				}
-
+				
 				return retVal;
 			}
 		};
@@ -4086,13 +4076,17 @@ AUI.add(
 				},
 
 				fillEmptyLocales(instance, fields, availableLanguageIds) {
-					fields.forEach(field => {
+					var defaultLocale = instance.getDefaultLocale();
+
+					if (availableLanguageIds.indexOf(defaultLocale) === -1){
+						availableLanguageIds.push(defaultLocale);
+					}
+
+					fields.forEach((field) => {
 						if (field.get('localizable')) {
 							var localizationMap = field.get('localizationMap');
 
-							var defaultLocale = field.getDefaultLocale();
-
-							availableLanguageIds.forEach(locale => {
+							availableLanguageIds.forEach((locale) => {
 								if (!localizationMap[locale]) {
 									localizationMap[locale] =
 										localizationMap[defaultLocale];
