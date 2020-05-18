e17f4a8a75c175b0dca685d3a910836a9f019cc0
Julien Castelain <julien.castelain@liferay.com>
2020-05-18 15:06:16 +0200

    LPS-113690 Add a pasteFilter to the description editor's configuration

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/editor/configuration/JournalArticleDescriptionEditorConfigContributor.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/editor/configuration/JournalArticleDescriptionEditorConfigContributor.java
@@ -50,6 +50,8 @@ public class JournalArticleDescriptionEditorConfigContributor
 		jsonObject.put(
 			"allowedContent", "p br strong i ol ul li u link pre em a"
 		).put(
+			"pasteFilter", "p br strong i ol ul li u link pre em a"
+		).put(
 			"toolbars", getToolbarsJSONObject(themeDisplay.getLocale())
 		);
 	}
