b95f54fe6a5ad71b509202513d94cffa4853d04c
Leonardo Barros <leonardo.barros@liferay.com>
2020-05-19 10:23:43 -0300

    LPS-113755 SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/internal/exportimport/data/handler/DDMStructureStagedModelDataHandler.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/internal/exportimport/data/handler/DDMStructureStagedModelDataHandler.java
@@ -330,10 +330,9 @@ public class DDMStructureStagedModelDataHandler
 
 		long groupId = portletDataContext.getScopeGroupId();
 
-		boolean stagingSite = MapUtil.getBoolean(
-			portletDataContext.getParameterMap(), "stagingSite");
+		if (MapUtil.getBoolean(
+				portletDataContext.getParameterMap(), "stagingSite")) {
 
-		if (stagingSite) {
 			Group group = _groupLocalService.fetchGroup(groupId);
 
 			if (group.isStaged() && !group.isStagingGroup()) {
