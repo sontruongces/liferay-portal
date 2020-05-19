decf4192f710aaff5c6b5d42a32b8c5b69b9d55b
drewbrokke <drew.brokke@liferay.com>
2020-05-19 09:56:20 -0500

    LPS-113096 portal-impl: SF - returns null when the languageId is null

--- portal-impl/src/com/liferay/portal/service/impl/VirtualHostLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/VirtualHostLocalServiceImpl.java
@@ -176,12 +176,9 @@ public class VirtualHostLocalServiceImpl
 
 			String languageId = hostnames.get(curHostname);
 
-			Locale locale;
+			Locale locale = LocaleUtil.fromLanguageId(languageId, true, false);
 
-			if (languageId != null) {
-				locale = LocaleUtil.fromLanguageId(languageId);
-			}
-			else {
+			if (locale == null) {
 				locale = LocaleUtil.getSiteDefault();
 			}
 
