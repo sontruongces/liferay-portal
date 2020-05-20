f77bdbec5c96ffa7db0334faf89c476c190d38da
Minhchau <minhchau.dang@liferay.com>
2020-05-20 11:46:25 +0900

    LPS-113622 Add parentheses around all the OR clauses to group them as one unit

--- portal-impl/src/com/liferay/portal/service/persistence/impl/RoleFinderImpl.java
+++ portal-impl/src/com/liferay/portal/service/persistence/impl/RoleFinderImpl.java
@@ -1155,7 +1155,9 @@ public class RoleFinderImpl extends RoleFinderBaseImpl implements RoleFinder {
 			return StringPool.BLANK;
 		}
 
-		StringBundler sb = new StringBundler(groupIds.length * 3 - 1);
+		StringBundler sb = new StringBundler(groupIds.length * 3 + 1);
+
+		sb.append(StringPool.OPEN_PARENTHESIS);
 
 		for (int i = 0; i < groupIds.length; i++) {
 			sb.append(table);
@@ -1166,6 +1168,8 @@ public class RoleFinderImpl extends RoleFinderBaseImpl implements RoleFinder {
 				sb.append(" OR ");
 			}
 		}
+
+		sb.append(StringPool.CLOSE_PARENTHESIS);
 
 		return sb.toString();
 	}
