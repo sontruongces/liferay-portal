b0f6206f13e36b0a768355e493ca631e77e562dc
Ivica Cardic <ivica.cardic@liferay.com>
2020-05-28 17:00:08 +0200

    COMMERCE-3838 - osb-commerce-provisioning-web -  Add commands for trial registration steps

---
+++ modules/dxp/apps/osb/osb-commerce/osb-commerce-provisioning/osb-commerce-provisioning-web/src/main/java/com/liferay/osb/commerce/provisioning/web/internal/portlet/action/PortalInstanceInitializedMVCRenderCommand.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.commerce.provisioning.web.internal.portlet.action;
+
+import com.liferay.commerce.model.CommerceSubscriptionEntry;
+import com.liferay.commerce.service.CommerceSubscriptionEntryLocalService;
+import com.liferay.osb.commerce.provisioning.constants.OSBCommercePortalInstanceConstants;
+import com.liferay.osb.commerce.provisioning.web.internal.constants.OSBCommerceProvisioningPortletKeys;
+import com.liferay.osb.commerce.provisioning.web.internal.util.OSBCommerceProvisioningWebKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Ivica Cardic
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + OSBCommerceProvisioningPortletKeys.TRIAL_REGISTRATION,
+		"mvc.command.name=portalInstanceInitialized"
+	},
+	service = MVCRenderCommand.class
+)
+public class PortalInstanceInitializedMVCRenderCommand
+	implements MVCRenderCommand {
+
+	@Override
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		long commerceOrderItemId = ParamUtil.getLong(
+			renderRequest, "commerceOrderItemId");
+
+		renderRequest.setAttribute(
+			OSBCommerceProvisioningWebKeys.PORTAL_INSTANCE_URL,
+			_getPortalInstanceVirtualHostname(
+				_getPortalInstanceVirtualHostname(commerceOrderItemId)));
+
+		return "/trial-registration/portal_instance_initialized.jsp";
+	}
+
+	private String _getPortalInstanceVirtualHostname(long commerceOrderItemId) {
+		CommerceSubscriptionEntry commerceSubscriptionEntry =
+			_commerceSubscriptionEntryLocalService.
+				fetchCommerceSubscriptionEntryByCommerceOrderItemId(
+					commerceOrderItemId);
+
+		UnicodeProperties unicodeProperties =
+			commerceSubscriptionEntry.getSubscriptionTypeSettingsProperties();
+
+		return unicodeProperties.get(
+			OSBCommercePortalInstanceConstants.
+				PORTAL_INSTANCE_VIRTUAL_HOSTNAME);
+	}
+
+	private String _getPortalInstanceVirtualHostname(
+		String portalInstanceVirtualHostname) {
+
+		boolean devEnvironment = _isMockEnvironment(
+			portalInstanceVirtualHostname);
+
+		return StringBundler.concat(
+			devEnvironment ? "http" : "https", "://",
+			portalInstanceVirtualHostname, ":", devEnvironment ? "8080" : "443",
+			"/group/osb-commerce");
+	}
+
+	private boolean _isMockEnvironment(String portalInstanceVirtualHostname) {
+		return portalInstanceVirtualHostname.endsWith(".test");
+	}
+
+	@Reference
+	private CommerceSubscriptionEntryLocalService
+		_commerceSubscriptionEntryLocalService;
+
+}
