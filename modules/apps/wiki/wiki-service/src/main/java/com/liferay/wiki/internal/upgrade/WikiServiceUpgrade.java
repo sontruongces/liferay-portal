dfc71b50f6a0af47a810ebcaef869e2bcdf80862
Samuel Ziemer <Samuel.Ziemer@liferay.com>
2020-05-29 10:56:44 -0700

    LPS-114612 Update usages

--- modules/apps/wiki/wiki-service/src/main/java/com/liferay/wiki/internal/upgrade/WikiServiceUpgrade.java
+++ modules/apps/wiki/wiki-service/src/main/java/com/liferay/wiki/internal/upgrade/WikiServiceUpgrade.java
@@ -14,6 +14,7 @@
 
 package com.liferay.wiki.internal.upgrade;
 
+import com.liferay.asset.kernel.service.AssetEntryLocalService;
 import com.liferay.comment.upgrade.UpgradeDiscussionSubscriptionClassName;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.settings.SettingsFactory;
@@ -61,8 +62,8 @@ public class WikiServiceUpgrade implements UpgradeStepRegistrator {
 		registry.register(
 			"1.1.0", "1.1.1",
 			new UpgradeDiscussionSubscriptionClassName(
-				_classNameLocalService, _subscriptionLocalService,
-				WikiPage.class.getName(),
+				_assetEntryLocalService, _classNameLocalService,
+				_subscriptionLocalService, WikiPage.class.getName(),
 				UpgradeDiscussionSubscriptionClassName.DeletionMode.UPDATE));
 
 		registry.register(
@@ -83,6 +84,9 @@ public class WikiServiceUpgrade implements UpgradeStepRegistrator {
 
 			});
 	}
+
+	@Reference
+	private AssetEntryLocalService _assetEntryLocalService;
 
 	@Reference
 	private ClassNameLocalService _classNameLocalService;
