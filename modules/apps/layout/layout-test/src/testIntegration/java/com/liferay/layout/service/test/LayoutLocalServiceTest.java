d79572609671dc99f12f65cb5afca00df0c1fd3f
balazssk <balazs.safrany-kovalik@liferay.com>
2020-06-08 09:29:55 +0200

    LPS-113902 Adding test both for current and legacy friendlyURLs

--- modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/service/test/LayoutLocalServiceTest.java
+++ modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/service/test/LayoutLocalServiceTest.java
@@ -66,6 +66,46 @@ public class LayoutLocalServiceTest {
 			ClassNameLocalServiceUtil.getClassNameId(User.class));
 	}
 
+	@Test
+	public void testLayoutsAreFoundBasedOnDoubleHttpEncodedLegacyFriendlyURL()
+		throws Exception {
+
+		String name = "café";
+
+		String friendlyURL = HttpUtil.decodeURL(StringPool.SLASH + name);
+
+		friendlyURL = HttpUtil.decodeURL(friendlyURL);
+
+		Layout layout = LayoutTestUtil.addLayout(
+			_group.getGroupId(), false,
+			Collections.singletonMap(LocaleUtil.getDefault(), name),
+			Collections.singletonMap(LocaleUtil.getDefault(), friendlyURL));
+
+		Assert.assertEquals(
+			layout,
+			_layoutLocalService.getFriendlyURLLayout(
+				_group.getGroupId(), false, friendlyURL));
+	}
+
+	@Test
+	public void testLayoutsAreFoundBasedOnHttpEncodedFriendlyURL()
+		throws Exception {
+
+		String name = "café";
+
+		String friendlyURL = HttpUtil.decodeURL(StringPool.SLASH + name);
+
+		Layout layout = LayoutTestUtil.addLayout(
+			_group.getGroupId(), false,
+			Collections.singletonMap(LocaleUtil.getDefault(), name),
+			Collections.singletonMap(LocaleUtil.getDefault(), null));
+
+		Assert.assertEquals(
+			layout,
+			_layoutLocalService.getFriendlyURLLayout(
+				_group.getGroupId(), false, friendlyURL));
+	}
+
 	@DeleteAfterTestRun
 	private Group _group;
 
