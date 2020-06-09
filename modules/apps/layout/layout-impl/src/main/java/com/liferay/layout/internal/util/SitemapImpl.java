7bf0470869aecea19e3575f54907408d87c289c4
Brian Chan <brian.chan@liferay.com>
2020-06-09 13:20:10 -0700

    LPS-92704 match entries and size (not entries and element size)

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/SitemapImpl.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/SitemapImpl.java
@@ -273,7 +273,7 @@ public class SitemapImpl implements Sitemap {
 		}
 	}
 
-	private int _getElementSize(Element element) {
+	private int _getSize(Element element) {
 		String string = element.asXML();
 
 		byte[] bytes = string.getBytes();
@@ -372,7 +372,7 @@ public class SitemapImpl implements Sitemap {
 	private void _initEntriesAndSize(Element rootElement) {
 		rootElement.addAttribute(_ENTRIES, "0");
 
-		int size = _getElementSize(rootElement);
+		int size = _getSize(rootElement);
 
 		rootElement.addAttribute(_SIZE, String.valueOf(size));
 	}
@@ -421,7 +421,7 @@ public class SitemapImpl implements Sitemap {
 		int size = GetterUtil.getInteger(rootElement.attributeValue(_SIZE));
 
 		entries++;
-		size += _getElementSize(newElement);
+		size += _getSize(newElement);
 
 		while ((entries > MAXIMUM_NUMBER_OF_ENTRIES) ||
 			   (size >= _MAXIMUM_SIZE_IN_BYTES)) {
@@ -430,7 +430,7 @@ public class SitemapImpl implements Sitemap {
 				newElement.getName());
 
 			entries--;
-			size -= _getElementSize(oldestUrlElement);
+			size -= _getSize(oldestUrlElement);
 
 			rootElement.remove(oldestUrlElement);
 		}
