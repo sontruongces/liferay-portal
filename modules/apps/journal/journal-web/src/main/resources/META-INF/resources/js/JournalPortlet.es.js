97e87fd512f3babb614af2dde9c5e565ce2645e7
Daniel Couso <daniel.couso@liferay.com>
2020-06-16 08:40:51 +0200

    LPS-115491 Use a different handler variable

--- modules/apps/journal/journal-web/src/main/resources/META-INF/resources/js/JournalPortlet.es.js
+++ modules/apps/journal/journal-web/src/main/resources/META-INF/resources/js/JournalPortlet.es.js
@@ -72,7 +72,7 @@ class JournalPortlet extends PortletBase {
 			this._onLocaleChange.bind(this)
 		);
 
-		this._localeChangedHandler = Liferay.after(
+		this._defaultLocaleChangedHandler = Liferay.after(
 			['inputLocalized:defaultLocaleChanged', 'ddm:default-locale-sync'],
 			this._onDefaultLocaleChange.bind(this)
 		);
@@ -93,7 +93,7 @@ class JournalPortlet extends PortletBase {
 	detached() {
 		this._eventHandler.removeAllListeners();
 		this._localeChangedHandler.detach();
-		this._onDefaultLocaleChange.detach();
+		this._defaultLocaleChangedHandler.detach();
 	}
 
 	/**
