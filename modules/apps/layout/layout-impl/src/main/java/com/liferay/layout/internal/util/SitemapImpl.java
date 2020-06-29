ac54e4a8a2e1a893c77879a46a31931b5e715533
Brian Chan <brian.chan@liferay.com>
2020-06-29 13:35:14 -0700

    LPS-92704 rename

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/SitemapImpl.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/SitemapImpl.java
@@ -197,7 +197,7 @@ public class SitemapImpl implements Sitemap {
 		if (Validator.isNull(layoutUuid) &&
 			PropsValues.XML_SITEMAP_INDEX_ENABLED) {
 
-			return _getSitemapIndex(groupId, privateLayout, themeDisplay);
+			return _getIndexSitemap(groupId, privateLayout, themeDisplay);
 		}
 
 		return _getSitemap(layoutUuid, groupId, privateLayout, themeDisplay);
@@ -321,7 +321,7 @@ public class SitemapImpl implements Sitemap {
 		return document.asXML();
 	}
 
-	private String _getSitemapIndex(
+	private String _getIndexSitemap(
 			long groupId, boolean privateLayout, ThemeDisplay themeDisplay)
 		throws PortalException {
 
