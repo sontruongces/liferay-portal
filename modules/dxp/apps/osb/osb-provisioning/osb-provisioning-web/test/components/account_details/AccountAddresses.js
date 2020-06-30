05adb3673eb20eb00d3294cddf97bfc596153674
Rebecca <rhdai10@gmail.com>
2020-06-29 18:28:20 -0700

    LHC-2443 add test cases for new component features

--- modules/dxp/apps/osb/osb-provisioning/osb-provisioning-web/test/components/account_details/AccountAddresses.js
+++ modules/dxp/apps/osb/osb-provisioning/osb-provisioning-web/test/components/account_details/AccountAddresses.js
@@ -14,13 +14,30 @@ import React from 'react';
 
 import AccountAddress from '../../../src/main/resources/META-INF/resources/js/components/account_details/AccountAddresses';
 
+const singleAddress = {
+	addresses: [
+		{
+			addressCountry: 'United States',
+			addressLocality: 'Diamond Bar',
+			addressRegion: 'California',
+			deletePostalAddressURL: '/',
+			editPostalAddressURL: '/',
+			id: '123',
+			postalCode: '91765',
+			streetAddressLine1: '1400 Montefino Ave',
+			streetAddressLine2: '-',
+			streetAddressLine3: '-'
+		}
+	]
+};
+
 function renderAccountAddress(props) {
 	return render(
 		<AccountAddress
 			accountKey="key123"
 			addresses={[
 				{
-					addressCountry: 'USA',
+					addressCountry: 'United States',
 					addressLocality: 'Diamond Bar',
 					addressRegion: 'California',
 					deletePostalAddressURL: '/',
@@ -67,10 +84,22 @@ describe('AccountAddress', () => {
 		Liferay.Service.mockImplementation(() =>
 			Promise.resolve([
 				{
-					countryId: '1',
-					name: 'Texas',
-					nameCurrentValue: 'Argentina',
-					regionId: '2',
+					countryId: '2',
+					name: 'Liaoning',
+					nameCurrentValue: 'China',
+					regionId: '2019',
+					zipRequired: true
+				},
+				{
+					countryId: '217',
+					nameCurrentValue: 'United Arab Emirates',
+					zipRequired: false
+				},
+				{
+					countryId: '19',
+					name: 'California',
+					nameCurrentValue: 'United States',
+					regionId: '19005',
 					zipRequired: true
 				}
 			])
@@ -149,23 +178,143 @@ describe('AccountAddress', () => {
 		return wait(() => expect(getAllByLabelText('delete').length).toBe(3));
 	});
 
-	it('displays Argentina as a country option when the user clicks on a Country field', () => {
-		const {getByText} = renderAccountAddress();
+	it('displays all address fields as editable when any one of the address fields is clicked', () => {
+		const {container, getByText} = renderAccountAddress();
 
-		fireEvent.click(getByText('USA'));
+		fireEvent.click(getByText('Building 8'));
 
 		return wait(() => {
-			getByText('Argentina');
+			expect(container.querySelectorAll('select').length).toBe(2);
+			expect(container.querySelectorAll('input[type=text]').length).toBe(
+				5
+			);
+
+			getByText('save');
+			getByText('cancel');
 		});
 	});
 
-	it('displays Texas as a region option when the user clicks on a Region field', () => {
-		const {getByText} = renderAccountAddress();
+	it('restores all address fields when the cancel button is clicked', () => {
+		const {container, getByText, queryByText} = renderAccountAddress();
 
-		fireEvent.click(getByText('California'));
+		fireEvent.click(getByText('Building 8'));
+		fireEvent.click(getByText('cancel'));
 
 		return wait(() => {
-			getByText('Texas');
+			expect(container.querySelectorAll('select').length).toBe(0);
+			expect(container.querySelectorAll('input[type=text]').length).toBe(
+				0
+			);
+
+			expect(queryByText('save')).toBeFalsy();
+			expect(queryByText('cancel')).toBeFalsy();
+		});
+	});
+
+	it('displays PRC, UAE, and USA as country options when the user clicks on a Country field', () => {
+		const {getByText} = renderAccountAddress(singleAddress);
+
+		fireEvent.click(getByText('United States'));
+
+		return wait(() => {
+			getByText('China');
+			getByText('United Arab Emirates');
+			getByText('United States');
+		});
+	});
+
+	it('displays country specific region options when a Country Field with valid Regions is selected', () => {
+		const {getByText} = renderAccountAddress(singleAddress);
+
+		return wait(() => {
+			fireEvent.change(getByText('United States'), {
+				target: {value: 'China'}
+			});
+
+			getByText('California');
+		});
+	});
+
+	it('displays an asterisk next to postal code when one is required for the selected Country field', () => {
+		const {getByText} = renderAccountAddress(singleAddress);
+
+		return wait(() => {
+			getByText('*');
+		});
+	});
+
+	it('keeps the Save button disabled when a required Postal Code field contains no value', () => {
+		const {getByText} = renderAccountAddress({
+			addresses: [
+				{
+					addressCountry: 'United States',
+					addressLocality: 'Diamond Bar',
+					addressRegion: 'California',
+					deletePostalAddressURL: '/',
+					editPostalAddressURL: '/',
+					id: '123',
+					postalCode: '-',
+					streetAddressLine1: '1400 Montefino Ave',
+					streetAddressLine2: '-',
+					streetAddressLine3: '-'
+				}
+			]
+		});
+
+		fireEvent.click(getByText('Diamond Bar'));
+
+		return wait(() => {
+			expect(getByText('save').disabled).toBeTruthy();
+		});
+	});
+
+	it('displays no asterisk next to Postal Code field when one is not required for the selected Country field', () => {
+		const {getByText, queryByText} = renderAccountAddress({
+			addresses: [
+				{
+					addressCountry: 'United Arab Emirates',
+					addressLocality: 'Dubai Media City',
+					addressRegion: '-',
+					deletePostalAddressURL: '/',
+					editPostalAddressURL: '/',
+					id: '456',
+					postalCode: '-',
+					streetAddressLine1: 'Building 8',
+					streetAddressLine2: 'Office 207',
+					streetAddressLine3: '-'
+				}
+			]
+		});
+
+		fireEvent.click(getByText('Office 207'));
+
+		return wait(() => {
+			expect(queryByText('*')).toBeFalsy();
+		});
+	});
+
+	it('allows Save button to be clickable when Postal Code field is empty for a selected Country field that does not require postal code', () => {
+		const {getByText} = renderAccountAddress({
+			addresses: [
+				{
+					addressCountry: 'United Arab Emirates',
+					addressLocality: 'Dubai Media City',
+					addressRegion: '-',
+					deletePostalAddressURL: '/',
+					editPostalAddressURL: '/',
+					id: '456',
+					postalCode: '-',
+					streetAddressLine1: 'Building 8',
+					streetAddressLine2: 'Office 207',
+					streetAddressLine3: '-'
+				}
+			]
+		});
+
+		fireEvent.click(getByText('Office 207'));
+
+		return wait(() => {
+			expect(getByText('save').disabled).toBeFalsy();
 		});
 	});
 });
