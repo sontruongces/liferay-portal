51574a7386d946c9e5cede0127b5d48bc3e6b691
IstvanD <istvan.dezsi@liferay.com>
2020-07-03 17:00:09 +0200

    LPS-116103 ItemSelectorKeyUtil is needed for the modified test

--- modules/apps/item-selector/item-selector-web/src/test/java/com/liferay/item/selector/web/internal/ItemSelectorImplTest.java
+++ modules/apps/item-selector/item-selector-web/src/test/java/com/liferay/item/selector/web/internal/ItemSelectorImplTest.java
@@ -21,6 +21,7 @@ import com.liferay.item.selector.ItemSelectorView;
 import com.liferay.item.selector.ItemSelectorViewRenderer;
 import com.liferay.item.selector.constants.ItemSelectorPortletKeys;
 import com.liferay.item.selector.web.internal.util.ItemSelectorCriterionSerializerImpl;
+import com.liferay.item.selector.web.internal.util.ItemSelectorKeyUtil;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.json.JSONFactoryImpl;
