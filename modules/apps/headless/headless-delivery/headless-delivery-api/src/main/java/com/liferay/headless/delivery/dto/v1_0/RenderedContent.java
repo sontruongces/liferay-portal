adaaf132a84e2f6b4cb16a6ab8d567d33067f11f
Javier Gamarra <nhpatt@gmail.com>
2020-07-17 23:41:11 +0200

    LPS-104929 generate

--- modules/apps/headless/headless-delivery/headless-delivery-api/src/main/java/com/liferay/headless/delivery/dto/v1_0/RenderedContent.java
+++ modules/apps/headless/headless-delivery/headless-delivery-api/src/main/java/com/liferay/headless/delivery/dto/v1_0/RenderedContent.java
@@ -79,6 +79,34 @@ public class RenderedContent {
 	@JsonProperty(access = JsonProperty.Access.READ_WRITE)
 	protected String renderedContentURL;
 
+	@Schema
+	public String getRenderedContentValue() {
+		return renderedContentValue;
+	}
+
+	public void setRenderedContentValue(String renderedContentValue) {
+		this.renderedContentValue = renderedContentValue;
+	}
+
+	@JsonIgnore
+	public void setRenderedContentValue(
+		UnsafeSupplier<String, Exception> renderedContentValueUnsafeSupplier) {
+
+		try {
+			renderedContentValue = renderedContentValueUnsafeSupplier.get();
+		}
+		catch (RuntimeException re) {
+			throw re;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@GraphQLField
+	@JsonProperty(access = JsonProperty.Access.READ_WRITE)
+	protected String renderedContentValue;
+
 	@Schema(
 		description = "The name of the template used to render the content."
 	)
@@ -178,6 +206,20 @@ public class RenderedContent {
 			sb.append("\"");
 
 			sb.append(_escape(renderedContentURL));
+
+			sb.append("\"");
+		}
+
+		if (renderedContentValue != null) {
+			if (sb.length() > 1) {
+				sb.append(", ");
+			}
+
+			sb.append("\"renderedContentValue\": ");
+
+			sb.append("\"");
+
+			sb.append(_escape(renderedContentValue));
 
 			sb.append("\"");
 		}
