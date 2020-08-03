f78f57d5fd98ad54ecca791bd298e93273b0b63f
ricardocousolr <ricardo.couso@liferay.com>
2020-08-03 16:11:03 +0200

    LPS-118393 Set first long/integer/long option as defaultValue instead of 0

--- modules/apps/segments/segments-web/src/main/resources/META-INF/resources/js/components/criteria_sidebar/CriteriaSidebarCollapse.es.js
+++ modules/apps/segments/segments-web/src/main/resources/META-INF/resources/js/components/criteria_sidebar/CriteriaSidebarCollapse.es.js
@@ -51,6 +51,9 @@ function getDefaultValue(property) {
 	else if (type === PROPERTY_TYPES.BOOLEAN) {
 		defaultValue = 'true';
 	}
+	else if (type === PROPERTY_TYPES.INTEGER && options && options.length) {
+		defaultValue = options[0].value;
+	}
 	else if (type === PROPERTY_TYPES.INTEGER) {
 		defaultValue = 0;
 	}
