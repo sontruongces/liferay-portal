f1713230af823cb9518481b818c3d86c8c24f679
jesseyeh-liferay <jesse.yeh@liferay.com>
2020-08-14 10:13:31 -0700

    LPS-119091 Prevent arrow keys from moving editable fragments

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/fragment_entry_link/FragmentEntryLink.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/fragment_entry_link/FragmentEntryLink.es.js
@@ -284,50 +284,52 @@ class FragmentEntryLink extends Component {
 	 * @review
 	 */
 	_handleFragmentKeyUp(event) {
-		event.stopPropagation();
+		if (!this.activeItemType === FRAGMENTS_EDITOR_ITEM_TYPES.editable) {
+			event.stopPropagation();
 
-		const direction = getItemMoveDirection(event.keyCode);
-		const {fragmentEntryLinkRowType} = event.delegateTarget.dataset;
+			const direction = getItemMoveDirection(event.keyCode);
+			const {fragmentEntryLinkRowType} = event.delegateTarget.dataset;
 
-		if (direction) {
-			if (
-				fragmentEntryLinkRowType ===
-				FRAGMENTS_EDITOR_ROW_TYPES.sectionRow
-			) {
-				moveRow(
-					direction,
-					getFragmentRowIndex(
+			if (direction) {
+				if (
+					fragmentEntryLinkRowType ===
+					FRAGMENTS_EDITOR_ROW_TYPES.sectionRow
+				) {
+					moveRow(
+						direction,
+						getFragmentRowIndex(
+							this.layoutData.structure,
+							this.fragmentEntryLinkId
+						),
+						this.store,
+						this.layoutData.structure
+					);
+				}
+				else {
+					const column = getFragmentColumn(
 						this.layoutData.structure,
 						this.fragmentEntryLinkId
-					),
-					this.store,
-					this.layoutData.structure
-				);
-			}
-			else {
-				const column = getFragmentColumn(
-					this.layoutData.structure,
-					this.fragmentEntryLinkId
-				);
-				const fragmentIndex = column.fragmentEntryLinkIds.indexOf(
-					this.fragmentEntryLinkId
-				);
-				const targetFragmentEntryLinkId =
-					column.fragmentEntryLinkIds[fragmentIndex + direction];
-
-				if (direction && targetFragmentEntryLinkId) {
-					const moveItemPayload = {
-						fragmentEntryLinkId: this.fragmentEntryLinkId,
-						targetBorder: getTargetBorder(direction),
-						targetItemId: targetFragmentEntryLinkId,
-						targetItemType: FRAGMENTS_EDITOR_ITEM_TYPES.fragment
-					};
-
-					moveItem(
-						this.store,
-						MOVE_FRAGMENT_ENTRY_LINK,
-						moveItemPayload
 					);
+					const fragmentIndex = column.fragmentEntryLinkIds.indexOf(
+						this.fragmentEntryLinkId
+					);
+					const targetFragmentEntryLinkId =
+						column.fragmentEntryLinkIds[fragmentIndex + direction];
+
+					if (direction && targetFragmentEntryLinkId) {
+						const moveItemPayload = {
+							fragmentEntryLinkId: this.fragmentEntryLinkId,
+							targetBorder: getTargetBorder(direction),
+							targetItemId: targetFragmentEntryLinkId,
+							targetItemType: FRAGMENTS_EDITOR_ITEM_TYPES.fragment
+						};
+
+						moveItem(
+							this.store,
+							MOVE_FRAGMENT_ENTRY_LINK,
+							moveItemPayload
+						);
+					}
 				}
 			}
 		}
