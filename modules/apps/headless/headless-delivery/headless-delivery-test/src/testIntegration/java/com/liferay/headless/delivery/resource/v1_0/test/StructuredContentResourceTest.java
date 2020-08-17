ee715f49bc9f88da4a0d25895ed4c904c6028ee9
Javier Gamarra <nhpatt@gmail.com>
2020-08-17 15:01:52 +0200

    LPS-102001 test for extensionContextResolver

--- modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/StructuredContentResourceTest.java
+++ modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/StructuredContentResourceTest.java
@@ -28,28 +28,45 @@ import com.liferay.dynamic.data.mapping.test.util.DDMTemplateTestUtil;
 import com.liferay.headless.delivery.client.dto.v1_0.ContentField;
 import com.liferay.headless.delivery.client.dto.v1_0.ContentFieldValue;
 import com.liferay.headless.delivery.client.dto.v1_0.StructuredContent;
+import com.liferay.headless.delivery.client.http.HttpInvoker;
 import com.liferay.headless.delivery.client.resource.v1_0.StructuredContentResource;
+import com.liferay.headless.delivery.client.serdes.v1_0.StructuredContentSerDes;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.journal.model.JournalFolder;
 import com.liferay.journal.test.util.JournalTestUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.template.TemplateConstants;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.vulcan.jaxrs.context.EntityExtensionContext;
+import com.liferay.portal.vulcan.jaxrs.context.ExtensionContext;
 
 import java.io.InputStream;
 
-import org.junit.After;
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.Map;
+import java.util.Set;
+
+import javax.ws.rs.ext.ContextResolver;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
 
 /**
  * @author Javier Gamarra
@@ -80,15 +97,15 @@ public class StructuredContentResourceTest
 			irrelevantGroup.getGroupId(), RandomTestUtil.randomString());
 	}
 
-	@After
 	@Override
-	public void tearDown() throws Exception {
-		super.tearDown();
-	}
-
 	@Test
-	public void testGetSiteStructuredContentWithDifferentLocale()
-		throws Exception {
+	public void testGetStructuredContent() throws Exception {
+
+		// Get structured content
+
+		super.testGetStructuredContent();
+
+		// Different locale
 
 		StructuredContent structuredContent =
 			structuredContentResource.postSiteStructuredContent(
@@ -123,6 +140,42 @@ public class StructuredContentResourceTest
 			structuredContent.getId());
 
 		Assert.assertEquals(title, structuredContent.getTitle());
+
+		// Extension
+
+		Bundle bundle = FrameworkUtil.getBundle(
+			StructuredContentResourceTest.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		Dictionary<String, String> properties = new HashMapDictionary<>();
+
+		properties.put(
+			"osgi.jaxrs.application.select",
+			"(osgi.jaxrs.name=Liferay.Headless.Delivery)");
+		properties.put("osgi.jaxrs.extension", "true");
+
+		ServiceRegistration<?> serviceRegistration =
+			bundleContext.registerService(
+				ContextResolver.class, new ExtensionContextResolver(),
+				properties);
+
+		structuredContent = structuredContentResource.postSiteStructuredContent(
+			testGroup.getGroupId(), randomStructuredContent());
+
+		HttpInvoker.HttpResponse httpResponse =
+			structuredContentResource.getStructuredContentHttpResponse(
+				structuredContent.getId());
+
+		String content = httpResponse.getContent();
+
+		Assert.assertTrue(content.contains("version"));
+
+		structuredContent = StructuredContentSerDes.toDTO(content);
+
+		Assert.assertNull(structuredContent.getTitle());
+
+		serviceRegistration.unregister();
 	}
 
 	@Override
@@ -143,6 +196,44 @@ public class StructuredContentResourceTest
 			structuredContentResource.
 				getStructuredContentRenderedContentTemplate(
 					structuredContent.getId(), _ddmTemplate.getTemplateId()));
+	}
+
+	public static class ExtensionContextResolver
+		implements ContextResolver<ExtensionContext> {
+
+		@Override
+		public ExtensionContext getContext(Class<?> type) {
+			if (com.liferay.headless.delivery.dto.v1_0.StructuredContent.class.
+					isAssignableFrom(type)) {
+
+				return new EntityExtensionContext
+					<com.liferay.headless.delivery.dto.v1_0.
+						StructuredContent>() {
+
+					@Override
+					public Map<String, Object> getEntityExtendedProperties(
+						com.liferay.headless.delivery.dto.v1_0.StructuredContent
+							structuredContent) {
+
+						return HashMapBuilder.<String, Object>put(
+							"version", "1.0"
+						).build();
+					}
+
+					@Override
+					public Set<String> getEntityFilteredPropertyKeys(
+						com.liferay.headless.delivery.dto.v1_0.StructuredContent
+							structuredContent) {
+
+						return Collections.singleton("title");
+					}
+
+				};
+			}
+
+			return null;
+		}
+
 	}
 
 	@Override
