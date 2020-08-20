81ad6d97bde230f256cdd85fd28854a327a193ec
Brian Chan <brian.chan@liferay.com>
2020-08-19 20:48:42 -0700

    LPS-119269 rename

--- modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/graphql/servlet/GraphQLServletExtender.java
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/graphql/servlet/GraphQLServletExtender.java
@@ -299,7 +299,7 @@ public class GraphQLServletExtender {
 				GraphQLType graphQLType = graphQLTypes.get(typeName);
 
 				if ((graphQLType != null) &&
-					!_typeKeys.contains(clazz.getName())) {
+					!_classNames.contains(clazz.getName())) {
 
 					String name = clazz.getName();
 
@@ -325,7 +325,7 @@ public class GraphQLServletExtender {
 
 				processingStack.push(typeName);
 
-				_typeKeys.add(clazz.getName());
+				_classNames.add(clazz.getName());
 
 				if (clazz.getAnnotation(GraphQLUnion.class) != null) {
 					graphQLType = new UnionBuilder(
@@ -467,7 +467,7 @@ public class GraphQLServletExtender {
 				return false;
 			}
 
-			private final Set<String> _typeKeys = new HashSet<>();
+			private final Set<String> _classNames = new HashSet<>();
 
 			{
 				setExtensionsHandler(graphQLExtensionsHandler);
