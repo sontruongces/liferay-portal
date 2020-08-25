3f36f2d45ea3a2da2797fc2e55c5b45639d38dd4
Dinh Thai <dinh.thai@codeenginestudio.com>
2020-08-25 11:50:40 +0700

    LPS-119037 Clicking on a user in the mentions list throws javascript error.

--- modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/_diffs/plugins/autocomplete/autocomplete.js
+++ modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/_diffs/plugins/autocomplete/autocomplete.js
@@ -19,6 +19,8 @@
 	var KeyMap = A.Event.KeyMap;
 	var Lang = A.Lang;
 
+	var BR_TAG = 'BR';
+
 	var CSS_LFR_AC_CONTENT = 'lfr-ac-content';
 
 	var STR_EDITOR = 'editor';
@@ -273,6 +275,16 @@
 			var prevTriggerPosition = instance._getPrevTriggerPosition();
 
 			var query = prevTriggerPosition.query;
+
+			if (
+				query &&
+				prevTriggerPosition.container.$.lastElementChild &&
+				prevTriggerPosition.container.$.lastElementChild.nodeName ===
+					BR_TAG
+			) {
+				query = null;
+			}
+
 			var trigger = prevTriggerPosition.value;
 
 			var res = instance._getRegExp().exec(query);
@@ -404,7 +416,7 @@
 				);
 			});
 
-			if (nextElement && nextElement.$.nodeName === 'BR') {
+			if (nextElement && nextElement.$.nodeName === BR_TAG) {
 				nextElement = null;
 			}
 
