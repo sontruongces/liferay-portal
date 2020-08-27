5018d83970d0452ecff4274f7cff6ff0124fb686
Rebecca <rhdai10@gmail.com>
2020-08-27 11:48:51 -0700

    LHC-2846 replace clay autocomplete with custom implementation

--- modules/dxp/apps/osb/osb-provisioning/osb-provisioning-web/src/main/resources/META-INF/resources/js/components/search/Search.js
+++ modules/dxp/apps/osb/osb-provisioning/osb-provisioning-web/src/main/resources/META-INF/resources/js/components/search/Search.js
@@ -11,6 +11,7 @@
 
 import ClayAutocomplete from '@clayui/autocomplete';
 import ClayDropDown from '@clayui/drop-down';
+import fuzzy from 'fuzzy';
 import debounce from 'lodash.debounce';
 import PropTypes from 'prop-types';
 import React, {useRef, useState} from 'react';
@@ -19,6 +20,45 @@ import {NAMESPACE} from '../../utilities/constants';
 import {request} from '../../utilities/helpers';
 
 const MAX_RESULTS = 7;
+
+const AutocompleteItem = React.forwardRef(
+	(
+		{innerRef, match = '', secondaryValue = '', value, ...otherProps},
+		ref
+	) => {
+		const fuzzyMatch = fuzzy.match(match, value);
+
+		return (
+			<ClayDropDown.Item {...otherProps} innerRef={innerRef} ref={ref}>
+				<>
+					{match && fuzzyMatch ? (
+						<div
+							className="main-item"
+							dangerouslySetInnerHTML={{
+								__html: fuzzyMatch.rendered
+							}}
+						/>
+					) : (
+						<div className="main-item">{value}</div>
+					)}
+
+					{!!secondaryValue && (
+						<div className="secondary-information">
+							{secondaryValue}
+						</div>
+					)}
+				</>
+			</ClayDropDown.Item>
+		);
+	}
+);
+
+AutocompleteItem.propTypes = {
+	href: PropTypes.string,
+	match: PropTypes.string,
+	secondaryValue: PropTypes.string,
+	value: PropTypes.string
+};
 
 function Search({accountsHomeURL = '', resourceURL}) {
 	const [error, setError] = useState(false);
@@ -95,10 +135,11 @@ function Search({accountsHomeURL = '', resourceURL}) {
 					<>
 						<ClayDropDown.ItemList>
 							{results.map(result => (
-								<ClayAutocomplete.Item
+								<AutocompleteItem
 									href={result.url}
 									key={result.key}
 									match={keywords}
+									secondaryValue={result.code}
 									value={result.name}
 								/>
 							))}
