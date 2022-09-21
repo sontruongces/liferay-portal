f540ec7a077592a09725c6e012f068552dc3a831
Thien Quach <thien.quach@codeenginestudio.com>
2022-09-21 17:07:06 +0700

    LPS-163007: Condition with custom integers not working correctly on Expression Builder

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-evaluator-impl/src/main/java/com/liferay/dynamic/data/mapping/form/evaluator/internal/function/EqualsFunction.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-evaluator-impl/src/main/java/com/liferay/dynamic/data/mapping/form/evaluator/internal/function/EqualsFunction.java
@@ -18,6 +18,7 @@ import com.liferay.dynamic.data.mapping.expression.DDMExpressionFunction;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 
 import java.math.BigDecimal;
@@ -35,7 +36,43 @@ public class EqualsFunction
 
 	@Override
 	public Boolean apply(Object object1, Object object2) {
-		return Objects.equals(_getValue(object1), _getValue(object2));
+		Object value1 = _getValue(object1);
+
+		Object value2 = _getValue(object2);
+
+		Object object1Class = value1.getClass();
+
+		Object object2Class = value2.getClass();
+
+		if (!object1Class.equals(object2Class) &&
+			((object1 instanceof BigDecimal) ||
+			 (object2 instanceof BigDecimal))) {
+
+			if (object1 instanceof BigDecimal) {
+				if (value2 instanceof Long) {
+					value1 = GetterUtil.getLong(value1);
+				}
+				else if (value2 instanceof Double) {
+					value1 = GetterUtil.getDouble(value1);
+				}
+				else if (value2 instanceof Integer) {
+					value1 = GetterUtil.getInteger(value1);
+				}
+			}
+			else {
+				if (value1 instanceof Long) {
+					value2 = GetterUtil.getLong(value2);
+				}
+				else if (value1 instanceof Double) {
+					value2 = GetterUtil.getDouble(value2);
+				}
+				else if (value1 instanceof Integer) {
+					value2 = GetterUtil.getInteger(value2);
+				}
+			}
+		}
+
+		return Objects.equals(value1, value2);
 	}
 
 	@Override
